c2bca49fe03cce0fdf393a6e49b9aa2a65f73946
Zhou Xianhua <xianhua.zhou@gmail.com>
2011-04-17 12:59:11 +0800

    a simple example with load balancer actors

---
+++ src/data/origin.php
@@ -0,0 +1,6 @@
+<?php
+for ($i = 0; $i <= 10; $i++) {
+  sleep(1);
+  echo time() . "\n";
+}
+?>
--- src/main/scala/study/zhou/App.scala
+++ src/main/scala/study/zhou/App.scala
@@ -1,14 +1,52 @@
 package study.zhou
 
-import com.caucho.quercus.QuercusEngine;
+import com.caucho.quercus.QuercusEngine
+import akka.actor.Actor
+import akka.actor.ActorRef
+
+import akka.routing.{ LoadBalancer, CyclicIterator }
+
+object Utils {
+  val engine = new QuercusEngine
+  val quercus = engine.getQuercus
+
+  def initPHPEngine {
+    quercus.init
+    quercus.start
+  }
+
+  def runPHPFile(file: String) {
+    engine.executeFile(file)
+  }
+
+  def runPHPCode(code: String) {
+    engine.execute(code)
+  }
+}
+
+class MyActor extends Actor {
+  def receive = {
+    case args: String => {
+      Utils.runPHPFile("/home/zhou/maven/sphp/src/data/my.php")
+    }
+  }
+}
+
+class LBActor extends Actor with LoadBalancer{
+  val actors = List.fill(3)(Actor.actorOf[MyActor].start)
+  val seq = new CyclicIterator[ActorRef](actors)
+}
 
 object App {
   def main(argv: Array[String]) {
-    println("hello")
-    val engine: QuercusEngine = new QuercusEngine
-    engine.getQuercus.init
-    engine.getQuercus.start
-    engine.executeFile("/home/zhou/test.php")
+    Utils.initPHPEngine
+
+    val lb = Actor.actorOf[LBActor].start
+    (1 to 10).foreach(x => 
+      lb ! " some args here "
+    )
+
+    argv.foreach(println)
   }
 }
 
